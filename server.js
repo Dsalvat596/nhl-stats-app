8afb08a574760f4a8d7a03560bcd1d3581d0d9a9
dsalvat596 <dsalvat596@gmail.com>
2019-02-20 15:45:53 -0500

    server

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ const app = express();
 
 app.use(express.static(__dirname,  'dist/hockey-app'));
 
-app.get('*', function(req,res) { 
+app.get('*', function(req, res) { 
     res.sendFile(path.join(__dirname, 'dist/hockey-app/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('running'));
