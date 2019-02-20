d1a7ce300bc72c7c26f6e320930b3efbd8e97fc9
dsalvat596 <dsalvat596@gmail.com>
2019-02-20 17:06:20 -0500

    ...

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const app = express();
 
 app.use(express.static(__dirname,  'dist/hockey-app'));
 
-app.all('*', function(req, res) { 
+app.get('*', function(req, res) { 
     res.sendFile(path.join(__dirname, 'dist/hockey-app/index.html'));
 });
 
-app.route('/*', function(req, res){
-    res.redirect(__dirname, 'dist/hockey-app/index.html');
-});
+// app.route('/*', function(req, res){
+//     res.redirect(__dirname, 'dist/hockey-app/index.html');
+// });
 
 const port = process.env.PORT || 3000;
 app.set('port', port);
