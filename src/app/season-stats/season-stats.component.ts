97566952e099694e81b0dbfba91386af08ec60eb
Daniel Salvatori <dsalvat596@gmail.com>
2019-12-27 15:20:17 -0500

    Update season-stats.component.ts
    updated for 2019-2020 season

--- src/app/season-stats/season-stats.component.ts
+++ src/app/season-stats/season-stats.component.ts
@@ -15,7 +15,7 @@ export class SeasonStatsComponent implements OnInit, OnDestroy {
   noStats: boolean = false;
   currentYearStats;
   sub: Subscription;
-  seasons = ['2018-2019','2017-2018','2016-2017','2015-2016','2014-2015','2013-2014','2012-2013','2011-2012','2010-2011','2009-2010','2008-2009']
+  seasons = ['2019-2020','2018-2019','2017-2018','2016-2017','2015-2016','2014-2015','2013-2014','2012-2013','2011-2012','2010-2011','2009-2010','2008-2009']
 
   constructor(private statsService: StatsService) { }
 
